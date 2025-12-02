07e511e64aaab97b0c48cbc6a0e4d37cd8f69adc
marzhendo <hexsazor@gmail.com>
2025-12-02 07:35:32 +0700

    new ver

--- main.go
+++ main.go
@@ -4,6 +4,20 @@ import "fmt"
 
 func main() {
 	//pointer and ref
-	var a = 1080
-	var *ptr = &a
+	var a int = 1080
+	var ptr *int = &a
+
+	fmt.Println("Value variabel a : ", a)
+	fmt.Println("Address variabel a : ", &a)
+	fmt.Println("Value pointer : ", *ptr)
+	fmt.Println("Address pointer : ", ptr)
+
+	//ref
+	fmt.Println("=====================")
+	var b string = "Antigravity"
+	var ref = &b
+	fmt.Println("Value variabel b : ", b)
+	fmt.Println("Address variabel b : ", &b)
+	fmt.Println("Value ref : ", *ref)
+	fmt.Println("Address ref : ", ref)
 }
